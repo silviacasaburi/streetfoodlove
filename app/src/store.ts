418e71b7b47d438392dd52d8ce59476b1b908481
silviacasaburi <72121503+silviacasaburi@users.noreply.github.com>
2022-01-16 22:12:42 -0800

    Delete app directory

--- app/src/store.ts
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {apiSlice} from './api';
-
-export default configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
-});
